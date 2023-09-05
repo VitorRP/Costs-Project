fb75f0f920158f04bf7735ed1bef3f99851a0e01
Vitor Ramos Pacheco <40059_vitor@callink.com.br>
2023-09-05 09:57:59 -0300

    Files updates

---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,13 @@
+import ProjectFrom from '../projects/ProjectForm'
+import styles from './NewProject.module.css'
+
+function NewProject() {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Create Poject</h1>
+            <p>Create your project and then add the services</p>
+            <ProjectFrom />
+        </div>
+    )
+}
+export default NewProject
